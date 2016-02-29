b2dea72d19b7ca6e63e9f6a5108aba36d0d07f9d
Loeller <serfozo.lorinc@live.com>
2016-02-29 23:18:03 +0100

    new: problem 1,2

---
+++ projectEuler_fsharp/problem_001.fsx
@@ -0,0 +1,1 @@
+﻿[0..999] |> List.filter (fun n -> (n%3=0) || (n%5=0)) |> List.sum
---
+++ projectEuler_fsharp/problem_002.fsx
@@ -0,0 +1,5 @@
+﻿(1,1)
+    |> Seq.unfold (fun (a, b) -> Some( a, ( b, a + b )))
+    |> Seq.takeWhile (fun n -> n <= 4000000)
+    |> Seq.filter (fun n -> n%2=0)
+    |> Seq.sum
