2381a9a23c585c313f0cabf649ecfbece64a636a
LLLL <j162261@trbvm.com>
2016-03-01 22:47:10 +0100

    problem_003

---
+++ projectEuler_fsharp/problem_003.fsx
@@ -0,0 +1,14 @@
+﻿open System
+
+let allDivisors (n: int64) =
+  let lim = n / 2L
+  [2L..lim] |> List.filter (fun x -> n % x = 0L)
+
+let isPrime (n:int64) =
+  allDivisors n = List.empty
+
+let maxPrimeDivisors (n:int64) =
+  let lim = int64(Math.Sqrt(float(n)))
+  [2L..lim] |> List.filter (fun x -> n % x = 0L && isPrime x) |> List.max
+
+maxPrimeDivisors 600851475143L
